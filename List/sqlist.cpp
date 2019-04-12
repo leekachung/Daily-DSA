c3253099c8e66e5448f77e0cd10e6d84e11c7a01
leekachung <leekachung17@gmail.com>
2019-04-12 17:14:44 +0800

    Add sqlist delete&location&output func

--- List/sqlist.cpp
+++ List/sqlist.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
+
 const int MAXSIZE = 100; // 线性表可能的最大长度 假设为100
 // 创建线性表
 typedef int datatype;    // 线性表节点的数据类型 假设为int
 const int maxsize = MAXSIZE;
-typedef struct
+typedef struct sqlist
 {
     datatype data[maxsize]; // 线性表的存储向量 第一个节点是data[0] 若第一个节点是data[1] 则声明为data[maxsize+1]
     int n;                  // 线性表的当前长度
-} sqlist;                   //顺序表类型
+} sqlist, *Psqlist;                   //顺序表类型
+
+// 初始化线性表
+void initList(sqlist *L)
+{
+    L->n = 0;
+}
+
+// 判断线性表是否为空
+bool isEmptyList(sqlist *L)
+{
+    return (L->n == 0);
+}
+
+// 判断线性表是否已满
+bool isFullList(sqlist *L)
+{
+    return (L->n == maxsize);
+}
 
 // 线性表插入数据
 // 将x插入到顺序表L的第i个位置
-int insert(sqlist *L, datatype x,int i) 
+int insData(sqlist *L, datatype x,int i) 
 {
-    if (L->n == maxsize) {std::cout<<"表满，不能插入！（上溢）\n"; return -1;}
+    if (isFullList(L)) {std::cout<<"表满，不能插入！（上溢）\n"; return -1;}
     if (i<1 || i>L->n+1) {std::cout<<"非法插入位置！\n"; return 0;}
-    for(int j = L->n; j >= i;j++)
+    for(int j = L->n; j >= i;j--)
     {
         L->data[j] = L->data[j-1]; // 结点后移
     }
@@ -24,7 +43,69 @@ int insert(sqlist *L, datatype x,int i)
     return 1; //插入成功
 }
 
+// 线性表删除数据
+// 删除第i个位置的元素
+int delData(sqlist *L, int i)
+{
+    if (isEmptyList(L)) {std::cout<<"表空，不能删除！\n"; return -1;}
+    if (i<1 || i>L->n+1) {std::cout<<"非法删除位置！\n"; return 0;}
+    for(int j = i; j < L->n; j++)
+    {
+        L->data[j-2] = L->data[j-1]; //第j个节点的下标为j-1
+    }
+    L->n--; //修改表长
+    return 1; // 删除成功
+    
+}
+
+// 线性表定位元素
+int locData(sqlist *L, datatype x)
+{
+    int i = 1;
+    while(i <= L->n && L->data[i-1] != x) i++;
+    // if (i <= L->n) return i;
+    // else return 0;
+    return i % (L->n + 1); //A Mod B = A-(A div B) * B
+}
+
+// 输出线性表
+void ouputList(sqlist *L)
+{
+    if (isEmptyList(L)) return; //表空返回
+    for (int i = 0; i < L->n; i++)
+    {
+        std::cout << locData(L, i);
+    }
+}
+
+// 顺序表中负值位于前面 正值位于后面
+void orderNum(sqlist *L)
+{
+    int x; //中间变量
+    int i = 0;
+    int j = L->n-1;
+    while (i < j) {
+        while (i < j && L->data[i] < 0) i++;
+        while (i < j && L->data[j] > 0) j--;
+        if (i < j) {
+            x = L->data[i];
+            L->data[i] = L->data[j];
+            L->data[j] = x;
+            i++;
+            j--;
+        }
+    }
+}
+
 int main()
 {
-    
+    // 初始化
+    sqlist L;
+    initList(&L);
+    // 插入数据
+    insData(&L, 0, 1);
+    ouputList(&L);
+    // 删除数据
+    delData(&L, 1);
+    ouputList(&L);
 }
